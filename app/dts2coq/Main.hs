a3737661406660460a53608e9d79816ad48bf6ea
Mai MATSUBARA <g2020537@is.ocha.ac.jp>
2024-01-22 06:08:55 +0900

    change Main.hs

--- app/dts2coq/Main.hs
+++ app/dts2coq/Main.hs
@@ -3,6 +3,7 @@
 
 --import Interface.Text 
 import qualified Data.Text.Lazy.IO as T
+import qualified Data.Text.Lazy as T
 import qualified DTS.UDTTdeBruijn as U
 import qualified DTS.UDTTvarName as D
 import qualified DTS.TypeQuery as TQ
@@ -15,14 +16,47 @@ sampleTypes :: [DTTpreterm]
 sampleTypes = [
   D.Sigma (D.VarName 'x' 1) (D.Con "entity") (D.App (D.Con "dog") (D.Var (D.VarName 'x' 1)))
   ]
-              
+
+
+
+--　test3用(Sigma)
+
+sigsample :: [(T.Text, U.Preterm U.DTT)]
+sigsample = [("A", U.Type), ("B", U.Type)]
+
+ctxsample :: [U.Preterm U.DTT]
+ctxsample = [U.Con "A", U.Sigma (U.Con "A") (U.Con "B")]
+
+typsample :: U.Preterm U.DTT
+typsample  = U.Con "B"
+
+
+
+-- test4用(Pi)
+sigsample' :: [(T.Text, U.Preterm U.DTT)]
+sigsample' = [("A", U.Type), ("B", U.Type)]
+
+ctxsample' :: [U.Preterm U.DTT]
+ctxsample' = [U.Pi (U.Con "A") (U.Con "B")]
+
+typsample' :: U.Preterm U.DTT
+typsample'  = U.Con "A"
+
+
+
 -- | The main function
 main :: IO ()
 main = do
   -- | test 1
   mapM_ (T.putStrLn . Coq.convcoq) sampleTypes
   -- | test 2
+  -- Coq.coqProver (TQ.ProofSearchSetting Nothing Nothing) (TQ.ProofSearchQuery [] [] (D.toDeBruijn [] D.Type))
+  -- | test 3
   Coq.coqProver (TQ.ProofSearchSetting Nothing Nothing)
-                (TQ.ProofSearchQuery [] [] (D.toDeBruijn [] D.Type))
+                (TQ.ProofSearchQuery sigsample ctxsample typsample)
+  -- | test 4
+  Coq.coqProver (TQ.ProofSearchSetting Nothing Nothing)
+                (TQ.ProofSearchQuery sigsample' ctxsample' typsample')
+
 
 
